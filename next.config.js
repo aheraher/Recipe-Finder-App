b608c55d619eb7dbb8509296a8fdd53e4ccbba8d
Dipak Aher <97633930+aheraher@users.noreply.github.com>
2023-11-17 01:43:39 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images :{
+        domains :['edamam-product-images.s3.amazonaws.com'],
+        // remotePatterns: [
+        //     {
+        //       protocol: 'https',
+        //       hostname: 'edamam-product-images.s3.amazonaws.com',
+        //       port: '',
+        //       pathname: '*/web-img/*/**', // Replace with the correct path pattern
+        //     },
+        //   ],
+       
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+}
 
 module.exports = nextConfig
--- src/app/componets/CardComponet.jsx
+++ src/app/componets/CardComponet.jsx
@@ -1,5 +1,28 @@
+// import React from 'react';
+// import styles from './CardComponent.module.css'; // Make sure to import the CSS file
+// import Image from 'next/image';
+
+// const CardComponent = ({ recipes }) => {
+//   return (
+//     <div className={styles.box}>
+//       {recipes.map((recipe, index) => (
+//         <div className={styles.card} key={index}>
+//           {/* <img src={recipe.recipe.image} alt={recipe.recipe.label} /> */}
+//           <Image src={recipe.recipe.image} width={200} height={150} alt={recipe.recipe.label} />
+//           <h2>{recipe.recipe.label}</h2>
+//           <h6>{recipe.dietLables} </h6>
+//           <p>Additional details or description here...</p>
+//           {/* You can include more details as needed */}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default CardComponent;
+
 import React from 'react';
-import styles from './CardComponent.module.css'; // Make sure to import the CSS file
+import styles from './CardComponent.module.css';
 import Image from 'next/image';
 
 const CardComponent = ({ recipes }) => {
@@ -7,10 +30,9 @@ const CardComponent = ({ recipes }) => {
     <div className={styles.box}>
       {recipes.map((recipe, index) => (
         <div className={styles.card} key={index}>
-          <img src={recipe.recipe.image} alt={recipe.recipe.label} />
-          {/* <Image src={recipe.recipe.image} width={200} height={150} alt={recipe.recipe.label} /> */}
+          <Image className= {styles.img} src={recipe.recipe.image} width={200} height={150} alt={recipe.recipe.label} />
           <h2>{recipe.recipe.label}</h2>
-          <h6>{recipe.dietLables} </h6>
+          <h6>{recipe.dietLables}</h6>
           <p>Additional details or description here...</p>
           {/* You can include more details as needed */}
         </div>
